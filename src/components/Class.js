cce66839671ba829f1449b8207c1e0b721d679e0
“Tommy” <nh3r717@gmail.com>
2020-03-09 08:05:19 +0800

    errors

---
+++ src/components/Class.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+// For smart components – passes data through a state.
+
+class ClassComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Class</h1>
+
+            <p>This is an example of using Class Export.</p>
+
+            </div>
+        );
+    }
+}
+export default ClassComponent;
---
+++ src/components/Export.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+//Exports multiple components from a single file – handles root components.
+
+export default class ExportComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Export</h1>
+            </div> 
+         );
+    }
+};
