48adb6814467f3203ad05791957d74cdf0331084
“Tommy” <nh3r717@gmail.com>
2020-03-21 07:55:00 +0800

    same issue as previous commit but uncommited

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 
-// import Newsfeed from '..src/pages/Newsfeed'
-// import Messages from '../src/pages/Messages'
+import Newsfeed from '..pages/Newsfeed'
+import Messages from '..pages/Messages'
 import Watch from '../pages/Watch'
 import Profile from '../pages/Profile'
 
@@ -14,8 +14,8 @@ class Routes extends Component {
             <section>
                 <Switch>
 
-                  {/* <Route exact path='/' component={Newsfeed} /> */}
-                  {/* <Route exact path='/Messages' component={Messages} /> */}
+                  <Route exact path='/' component={Newsfeed} />
+                  <Route exact path='/Messages' component={Messages} />
                   <Route exact path='/Watch' component={Watch} />
                   <Route exact path='/Profile' component={Profile} />
 
