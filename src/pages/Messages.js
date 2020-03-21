a3880f24120aeb5caac940fc61de360e2ed17690
Silvestro <csilvestro@csilvestrombp.fullsail.com>
2020-03-21 11:56:55 -0400

    Added notes for messages.

--- src/pages/Messages.js
+++ src/pages/Messages.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 
 class Messages extends Component {
+    state = {
+        //Create an array for 3 messages.
+    }
     render() {
+        //Loop through the messages array.
         return (       
             <section className="">
                 <h1>Messages</h1>
+                {/* Display messages array */}
             </section>
         );
     }
